80c9630748ad230431fc5d2eb7e05cb5d47c1eea
ChokSheak Lau <choksheak@dropbox.com>
2020-12-13 22:21:47 -0800

    Simplify code with minor fixes

--- index.js
+++ index.js
@@ -1,52 +1,46 @@
 #!/usr/bin/env node
 
 const spawn = require("cross-spawn");
-const npmRunPath = require('npm-run-path');
+const npmRunPath = require("npm-run-path");
 const colors = require("kleur/colors");
 
-const prefix = colors.dim("[timecli]") + " ";
+const prefix = colors.dim("[timecli] ");
+const localeOptions = { minimumFractionDigits: 3, maximumFractionDigits: 3 };
 
-const getTimeTaken = totalMs => {
-    const secNum = Math.floor(totalMs / 1000);
-    const secStr = secNum.toLocaleString();
-    const msMod = (totalMs % 1000).toString();
-    const msStr = "00".substring(msMod.length - 1) + msMod;
-    let output = colors.cyan(secStr + "." + msStr + "s");
+const num3dp = (n) => n.toLocaleString("en-US", localeOptions);
 
-    if (secNum >= 60) {
-        const minsNum = Math.floor(secNum / 60).toLocaleString();
-        const secMod = secNum % 60;
-        output = colors.yellow(minsNum + "m " + secMod + "s") + " (" + output + ")";
-    }
+const getTimeTaken = (totalMs) => {
+  const secNum = totalMs / 1000;
+  const secStr = num3dp(secNum);
+  let output = colors.cyan(secStr + "s");
 
-    return output;
+  if (secNum >= 60) {
+    const minsNum = Math.floor(secNum / 60).toLocaleString();
+    const secMod = num3dp(secNum % 60);
+    output = colors.yellow(minsNum + "m " + secMod + "s") + " (" + output + ")";
+  }
+
+  return output;
 };
 
-const runCommand = async args => {
-    const startMillis = Date.now();
+const runCommand = async (args) => {
+  const startMillis = Date.now();
 
-    const exitCode = await new Promise(resolve => {
-        const childProcess = spawn(args[0], args.slice(1), {
-            env: npmRunPath.env({
-                env: process.env,
-                cwd: process.cwd(),
-                execPath: process.execPath
-            }),
-            stdio: "inherit"
-        });
-
-        childProcess.on('close', exitCode => {
-            resolve(exitCode);
-        });
+  const exitCode = await new Promise((resolve) => {
+    const childProcess = spawn(args[0], args.slice(1), {
+      env: npmRunPath.env(),
+      stdio: "inherit",
     });
 
-    const timeTaken = getTimeTaken(Date.now() - startMillis);
-    const exitCodeStr = `[Exit code = ${exitCode}]`;
-    const exitCodeColored = exitCode === 0
-        ? colors.green(exitCodeStr)
-        : colors.red(exitCodeStr);
+    childProcess.on("close", (exitCode) => resolve(exitCode));
+  });
 
-    console.log(`\n${prefix}Took ${timeTaken}. ${exitCodeColored}`);
+  const timeTaken = getTimeTaken(Date.now() - startMillis);
+  const exitCodeStr = `[Exit code = ${exitCode}]`;
+  const exitCodeColored =
+    exitCode === 0 ? colors.green(exitCodeStr) : colors.red(exitCodeStr);
+
+  console.log(`${prefix}Took ${timeTaken}. ${exitCodeColored}`);
 };
 
 const main = () => {
